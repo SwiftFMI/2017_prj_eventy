1b89bf713cfce05e0cb6c9036d7d229033a0ab12
Valentin Varbanov <valentin.varbanov@gmail.com>
2018-01-15 22:14:17 +0200

    Init of CocoaPods and a sample logging framework

--- Eventy/AppDelegate.swift
+++ Eventy/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import SwiftyBeaver
+
+let log = SwiftyBeaver.self
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // add log destinations. at least one is needed!
+        let console = ConsoleDestination()
+        
+        // add the destinations to SwiftyBeaver
+        log.addDestination(console)
+        
+        // Now let’s log!
+//        log.verbose("not so important")  // prio 1, VERBOSE in silver
+//        log.debug("something to debug")  // prio 2, DEBUG in green
+//        log.info("a nice information")   // prio 3, INFO in blue
+//        log.warning("oh no, that won’t be good")  // prio 4, WARNING in yellow
+//        log.error("ouch, an error did occur!")  // prio 5, ERROR in red
+
+        log.info("We are running")
+        
         return true
     }
 
