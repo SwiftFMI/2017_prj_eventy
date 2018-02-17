b845323ac5b1b55cfd8053379d098bb10450efcd
Ivan Ganchev <planins@ivans-mbp.lan>
2018-02-17 16:33:50 +0200

    Profile image and name in Profile View Controller

--- Eventy/ProfileViewController.swift
+++ Eventy/ProfileViewController.swift
@@ -6,29 +6,62 @@
 //
 
 import UIKit
+import Alamofire
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    var mainUser: User?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        loadProfile {}
     }
-
+    
+    
+    func loadProfile(completion: @escaping () -> Void) {
+        guard let t = token?.accessToken else { return }
+    
+        Alamofire.request(
+            URL(string: serverIp + "/userinfo")!,
+            method: .get,
+            headers: ["access-token" : t])
+            .validate()
+            .responseString { (response) -> Void in
+                guard response.result.isSuccess else {
+                    log.error("Error response: \(String(describing: response.result.error))")
+                    completion()
+                    return
+                }
+                
+                do {
+                    log.debug(response.result.value!)
+                    
+                    if let json = response.result.value {
+                        self.mainUser = try User(json)
+                        self.nameLabel.text = self.mainUser!.name
+                        let profilePicUrl = URL(string: self.mainUser!.profilePicPath)
+                        let imageData: Data = try! Data(contentsOf: profilePicUrl!)
+                        self.profileImage.image = UIImage(data: imageData)
+                        
+                    } else {
+                        log.warning("cound not get json")
+                    }
+                } catch {
+                    log.warning("Cound not parse response")
+                }
+                completion()
+        }
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
